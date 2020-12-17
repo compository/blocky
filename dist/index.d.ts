ae84ba2de6d29749764f2f1065d35aeeb5d0ecba
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-12-17 11:05:36 +0100

    Updates

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,1 @@
-/**
- * Process
- * 0. Get the dna hash
- * 1. Get the wasm hashes for each of the zomes
- * 2. Fetch the UI bundles for each of the zomes
- * 3. Set the available blocks as the
- * 4. If there is some block already defined in the zome, download it and render it
- *   4.1 Else, display layout editor and save layout to the zome
- */
+export * from './elements/blocky-block-board';
