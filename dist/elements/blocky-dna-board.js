1a4842efcc2e7909ed676ff875d3c39dd844164b
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-12-30 18:46:57 +0100

    Updates

--- dist/elements/blocky-dna-board.js
+++ dist/elements/blocky-dna-board.js
@@ -90,7 +90,7 @@ export class BlockyDnaBoard extends membraneContext(Scoped(LitElement)) {
           icon="save"
           slot="actionItems"
           label="Save Layout"
-          .disabled=${!this.board.getEditingLayout()}
+          .disabled=${!this.board || this.board.isEditingLayoutEmpty()}
           @click=${() => {
                 const newLayout = this.board.save();
                 this.createBoard(newLayout);
@@ -132,30 +132,30 @@ export class BlockyDnaBoard extends membraneContext(Scoped(LitElement)) {
           .editing=${this._editing}
           .blockSets=${this._blockSets}
           ?blockLayout=${this._blockNode}
+          @layout-updated=${() => this.requestUpdate()}
         ></block-board>
       `;
     }
     render() {
         return html `<membrane-context-provider
-        .appWebsocket=${this.membraneContext.appWebsocket}
-        .cellId=${this.cellIdToDisplay}
-      >
-        <mwc-top-app-bar style="flex: 1; display: flex;">
-          <mwc-icon-button
-            icon="arrow_back"
-            slot="navigationIcon"
-            @click=${() => this.dispatchEvent(new CustomEvent('navigate-back'))}
-          ></mwc-icon-button>
-          <div slot="title">${serializeHash(this.cellIdToDisplay[0])}</div>
-  
-          ${this.renderBarItems()}
+      .appWebsocket=${this.membraneContext.appWebsocket}
+      .cellId=${this.cellIdToDisplay}
+    >
+      <mwc-top-app-bar style="flex: 1; display: flex;">
+        <mwc-icon-button
+          icon="arrow_back"
+          slot="navigationIcon"
+          @click=${() => this.dispatchEvent(new CustomEvent('navigate-back'))}
+        ></mwc-icon-button>
+        <div slot="title">${serializeHash(this.cellIdToDisplay[0])}</div>
 
-          <div style="width: 100vw; height: 100%; display: flex;">
-            ${this.renderContent()}
-              </div>
-          </div>
-        </mwc-top-app-bar>
-      </membrane-context-provider>`;
+        ${this.renderBarItems()}
+
+        <div style="width: 100vw; height: 100%; display: flex;">
+          ${this.renderContent()}
+        </div>
+      </mwc-top-app-bar>
+    </membrane-context-provider>`;
     }
     static get scopedElements() {
         return {
